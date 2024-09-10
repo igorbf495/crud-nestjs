1307dde129e37ecf0185e4c427c11ce6ced6709d
igorbf495 <igorcarvalhofelipe495@gmail.com>
2024-09-10 03:20:01 -0300

    create user document

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [UsersModule,MongooseModule.forRoot('mongodb+srv://felhacking:93wfSUVJb8ZLvwYA@cluster0.74slq.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
